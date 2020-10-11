1f6cb9fe10336d809c80edb3d5d7281fd8289dc7
TheExiledCat <43574582+TheExiledCat@users.noreply.github.com>
2020-10-11 23:22:55 +0200

    movement done for tetris blocks

---
+++ Plinkis/Assets/Field.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    public static Field Grid;
+    public int width =10;
+    public int height=20;
+    
+    void Awake()
+    {
+        
+        if (Grid == null)
+        {
+            Grid = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        transform.localScale = new Vector3(width, height, 1);
+        transform.position += Vector3.right * width / 2+Vector3.up*height/2;
+        
+    }
+    void OnDrawGizmos()
+    {
+        
+        Gizmos.DrawWireCube(transform.position+new Vector3(width/2,height/2,1), new Vector3(width, height));
+    }
+}
---
+++ Plinkis/Assets/Tetronimo_Movement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetronimo_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float autoFloatTimer=0.8f;
+    [SerializeField] float horizontalTimer = 0.3f;
+    float lastTimer;
+    float lastHorizontalTimer;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveX(false);
+            lastHorizontalTimer = Time.time;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveX(true);
+            lastHorizontalTimer = Time.time;
+        }
+        if (Time.time - lastHorizontalTimer > horizontalTimer)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                MoveX(false);
+                lastHorizontalTimer = Time.time;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                MoveX(true);
+                lastHorizontalTimer = Time.time;
+            }
+        }
+        if (Time.time - lastTimer > (Input.GetKey(KeyCode.DownArrow) ? autoFloatTimer /10: autoFloatTimer)){
+            MoveY(true);
+            lastTimer = Time.time;
+        }
+    }
+    void MoveX(bool right)
+    {
+        transform.position += right ? Vector3.right : Vector3.left;
+        if (CheckOutOfBounds())
+        {
+            transform.position += right ? Vector3.left : Vector3.right;
+        }
+    }
+    void MoveY(bool down)
+    {
+        transform.position += down ? Vector3.down : Vector3.up;
+        if (CheckOutOfBounds())
+        {
+            transform.position += down ? Vector3.up : Vector3.down;
+        }
+    }
+    bool CheckOutOfBounds()
+    {
+        foreach(Transform t in transform)
+        {
+            int x = Mathf.RoundToInt(t.position.x);
+            int y = Mathf.RoundToInt(t.position.y);
+            
+
+            if (x <0 || x >= Field.Grid.width || y <0 || y >= Field.Grid.height)
+            {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+}
